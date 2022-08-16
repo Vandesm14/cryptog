29ac23334d97cc3d1349f0481921e51b24978acd
Vandesm14 <vandesm14@gmail.com>
2022-08-15 21:26:32 -0400

    next algorithm step

--- algorithm.md
+++ algorithm.md
@@ -1,7 +1,7 @@
 # Algorithm
 
-1. Find the smallest word
-2. Find the word within that list that has the most influence over the cryptogram
-3. Guess that word
-4. Guess all words that have at least 1 letter completed
-5. Order those guesses by frequency
+1. [x] Find the smallest word
+2. [x] Find the word within that list that has the most influence over the cryptogram
+3. [x] Guess that word
+4. [ ] Guess all words that have at least 1 letter completed
+5. [ ] Order those guesses by **frequency**
--- gen-cryptogram.ts
+++ gen-cryptogram.ts
@@ -1,5 +1,5 @@
 import { getMostInfluentialWord, getSmallestWords } from './crunch.ts';
-import { scramble } from './lib.ts';
+import { filter, scramble, words } from './lib.ts';
 
 const sentence =
   'The manager of the fruit stand always sat and only sold vegetables.'.replace(
@@ -8,7 +8,11 @@ const sentence =
   );
 
 const cryptogram = scramble(sentence);
-const words = cryptogram.split(' ');
 
 console.log(cryptogram);
-console.log(getMostInfluentialWord(words, getSmallestWords(words)));
+const firstWord = getMostInfluentialWord(
+  cryptogram.split(' '),
+  getSmallestWords(cryptogram.split(' '))
+);
+
+const guess = filter(words, [], '.'.repeat(firstWord.length))[0];
